800b0e1655937551f0bbfd5805d473958392cbc4
truong.le@seta-international.vn <truong.le@seta-international.vn>
2025-01-22 12:11:39 +0700

    feat: add proptypes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,60 +1,67 @@
-/* eslint-disable react/prop-types */
 import { FaToggleOff, FaToggleOn, FaTrash } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { removeTodo, toggleTodo, updateTodo } from "../state/todo/slice";
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 export const TodoItem = ({ todo }) => {
-  const dispatch = useDispatch();
-  const [isEditing, setIsEditing] = useState(false); 
-  const [newText, setNewText] = useState(todo.text); 
+    TodoItem.propTypes = {
+        todo: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            completed: PropTypes.bool.isRequired,
+        }).isRequired,
+    };
+    const dispatch = useDispatch();
+    const [isEditing, setIsEditing] = useState(false); 
+    const [newText, setNewText] = useState(todo.text); 
 
-  const handleUpdate = () => {
-    if (newText.trim()) {
-      dispatch(updateTodo({ id: todo.id, text: newText.trim() }));
-      setIsEditing(false); 
-    }
-  };
+    const handleUpdate = () => {
+        if (newText.trim()) {
+        dispatch(updateTodo({ id: todo.id, text: newText.trim() }));
+        setIsEditing(false); 
+        }
+    };
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      handleUpdate();
-    } else if (e.key === "Escape") {
-      setIsEditing(false); 
-      setNewText(todo.text);
-    }
-  };
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+        handleUpdate();
+        } else if (e.key === "Escape") {
+        setIsEditing(false); 
+        setNewText(todo.text);
+        }
+    };
 
-  return (
-    <li className="flex justify-between border-b-2 py-2 gap-4">
-      <div className="flex items-center" onClick={() => setIsEditing(true)} >
-        {isEditing ? (
-          <input
-            type="text"
-            value={newText}
-            onChange={(e) => setNewText(e.target.value)}
-            onKeyDown={handleKeyDown}
-            onBlur={handleUpdate} 
-            className="border rounded px-2 py-1 w-full"
-            autoFocus 
-          />
-        ) : (
-          <span
-            className={`${todo.completed ? "line-through text-red-500" : ""}`}
-            title="Double click to edit"
-          >
-            {todo.text}
-          </span>
-        )}
-      </div>
-      <div className="space-x-3">
-        <button onClick={() => dispatch(toggleTodo({ id: todo.id }))}>
-          {todo.completed ? <FaToggleOff className="size-5" /> : <FaToggleOn className="size-5" />}
-        </button>
-        <button onClick={() => dispatch(removeTodo({ id: todo.id }))}>
-          <FaTrash className="text-red-500 size-5" />
-        </button>
-      </div>
-    </li>
-  );
+    return (
+        <li className="flex justify-between border-b-2 py-2 gap-4">
+        <div className="flex items-center" onClick={() => setIsEditing(true)} >
+            {isEditing ? (
+            <input
+                type="text"
+                value={newText}
+                onChange={(e) => setNewText(e.target.value)}
+                onKeyDown={handleKeyDown}
+                onBlur={handleUpdate} 
+                className="border rounded px-2 py-1 w-full"
+                autoFocus 
+            />
+            ) : (
+            <span
+                className={`${todo.completed ? "line-through text-red-500" : ""}`}
+                title="Double click to edit"
+            >
+                {todo.text}
+            </span>
+            )}
+        </div>
+        <div className="space-x-3">
+            <button onClick={() => dispatch(toggleTodo({ id: todo.id }))}>
+            {todo.completed ? <FaToggleOff className="size-5" /> : <FaToggleOn className="size-5" />}
+            </button>
+            <button onClick={() => dispatch(removeTodo({ id: todo.id }))}>
+            <FaTrash className="text-red-500 size-5" />
+            </button>
+        </div>
+        </li>
+    );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,27 +1,30 @@
 import { useSelector } from "react-redux";
 import { TodoItem } from "./TodoItem";
 import { selectFilteredTodos } from "../state/todo/selector";
+import PropTypes from "prop-types";
 
-// eslint-disable-next-line react/prop-types
 const TodoList = ({ searchValue }) => {
-  const filteredTodos = useSelector(selectFilteredTodos);
+    TodoList.propTypes = {
+        searchValue: PropTypes.string,
+    };
+    const filteredTodos = useSelector(selectFilteredTodos);
 
-  const text = searchValue
-    ? filteredTodos.length > 0
-      ? `Notes containing "${searchValue}"`
-      : "Oops, cannot find your note"
-    : filteredTodos.length > 0
-    ? "All notes..."
-    : "Nothing to show";
+    const text = searchValue
+        ? filteredTodos.length > 0
+        ? `Notes containing "${searchValue}"`
+        : "Oops, cannot find your note"
+        : filteredTodos.length > 0
+        ? "All notes..."
+        : "Nothing to show";
 
-  return (
-    <ul>
-      <li className="my-2 text-sm italic">{text}</li>
-      {filteredTodos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+        <li className="my-2 text-sm italic">{text}</li>
+        {filteredTodos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} />
+        ))}
+        </ul>
+    );
 };
 
 export default TodoList;
