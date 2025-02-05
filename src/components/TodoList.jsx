91e08961d25c5001a934dcbca6dde77012db3bd3
truong.le@seta-international.vn <truong.le@seta-international.vn>
2025-02-05 14:36:31 +0700

    feat: implement drag and drop to list todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,17 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { TodoItem } from "./TodoItem";
 import { selectFilteredTodos } from "../state/todo/selector";
+import { reorderTodos } from "../state/todo/slice"; // ✅ Import action mới
 import PropTypes from "prop-types";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
 const TodoList = ({ searchValue }) => {
     TodoList.propTypes = {
         searchValue: PropTypes.string,
     };
-    const filteredTodos = useSelector(selectFilteredTodos);
+
+    const dispatch = useDispatch();
+    const filteredTodos = useSelector(selectFilteredTodos); // Lấy danh sách từ Redux
 
     const text = searchValue
         ? filteredTodos.length > 0
@@ -17,13 +21,48 @@ const TodoList = ({ searchValue }) => {
             ? "All notes..."
             : "Let’s add your note";
 
+    const handleOnDragEnd = (result) => {
+        if (!result.destination) return;
+
+        const newTodos = Array.from(filteredTodos);
+        const [draggedItem] = newTodos.splice(result.source.index, 1);
+        newTodos.splice(result.destination.index, 0, draggedItem);
+
+        // ✅ Dispatch action để cập nhật thứ tự mới vào Redux
+        dispatch(reorderTodos(newTodos));
+    };
+
     return (
-        <ul>
-            <li className="my-2 text-sm italic">{text}</li>
-            {filteredTodos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} />
-            ))}
-        </ul>
+        <div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId="todos">
+                    {(provided) => (
+                        <ul
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            <li className="my-2 text-sm italic">{text}</li>
+                            {filteredTodos.map((todo, index) => (
+                                <Draggable key={todo.id} draggableId={todo.id.toString()} index={index}>
+                                    {(provided) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            style={provided.draggableProps.style}
+                                            className="p-4 border rounded-md cursor-pointer bg-white"
+                                        >
+                                            <TodoItem key={todo.id} todo={todo} />
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </ul>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </div>
     );
 };
 
